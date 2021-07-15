8b3b1a39613857efb32f5f40abeba278f0122bb0
Marcos Daniel <marcos.mdbbr@gmail.com>
2021-07-15 13:03:53 -0300

    removido comentarios

--- src/main/java/com/marcos/ciss/model/Funcionario.java
+++ src/main/java/com/marcos/ciss/model/Funcionario.java
@@ -49,22 +49,4 @@ public class Funcionario {
     @Pattern(regexp = "[0-9]+", message = "Formato invalido, utilize apenas numeros para o atributo Numero Nis")
     private String numeroNis;
 
-    /**
-     * Dos atributos do Funcionário - Nome (Entre 2 e 30 caracteres) -
-     * Sobrenome(Entre 2 e 50 caracteres) - e-mail (Validar e-mail) - Número do NIS
-     * (PIS) (Somente números)
-     * 
-     * Das funcionalidades (Utilizando APIs): - Inserção de um Funcionário -
-     * Exclusão de um Funcionário - Atualização de um Funcionário
-     * 
-     * - Listagem de um Funcionário
-     * 
-     * 
-     * Do desenvolvimento: - Utilize o padrão de desenvolvimento que acredita ser o
-     * mais correto.
-     * 
-     * Da entrega dos fontes: - Faça a entrega dos fontes da forma que acredita ser
-     * a mais correta.
-     */
-
 }
